e3cc23b4d0ddb9137e1aa7e4ef1f03de8bd9215c
Lauren E. Hostert <40471257+lehostert@users.noreply.github.com>
2019-04-30 16:58:33 -0500

    Remove old selection calls

--- Site_Selection/Random_Stratified_Site_Selection.R
+++ Site_Selection/Random_Stratified_Site_Selection.R
@@ -1,10 +1,8 @@
-#Script for CREP Random Site selection 2018. This begins CREP phase III site selection. 
+#Script for CREP Random Site selection 2018. This begins CREP phase III site selection.
 
-install.packages("dplyr")
-library(dplyr)
-attach(dplyr)
+library(tidyverse)
 
-df <- read.csv("PU_Gaps_size_and_CRP_classes.csv")
+df <- read_csv("C:/Users/lhostert/Documents/GitHub/Kaskaskia-River-CREP-Monitoring-Data-Tools/Site_Selection/Data/Kaskaskia_Catchment_Sizes_and_Features.csv")
 year <- 2019
 
 
@@ -18,18 +16,20 @@ year <- 2019
 #   group_by(basin, size_class, CRP_class) %>%
 #   sample_n(5)
 
-## Random Sites selected- 8 sites per each basin, size, CRP combo due to the limits of each category.
-## We will only sample 5 per basin but an extra 3 are chosen to provide  backup for impossible sites.
+## For 2018 onward
+## Random Sites selected- 6 sites per each basin, size combo due to the limits of each category.
+## We will only sample 5 per basin (3 small, 2 large) but an extra 3 or 4 are chosen to provide  backup for impossible sites.
 set.seed(year)
 sites <-  df %>%
-  group_by(basin, size_class) %>%
-  sample_n(8, replace = TRUE)
+  group_by(HUC8_Basin_Name, size_class) %>%
+  sample_n(6, replace = TRUE)
 
 
 ##Export to .csv
 
 write.csv(sites, file = paste0("C:/Users/lhostert/Documents/CREP/R_Scripts/Sites/", year,"_SiteSelection.csv"), row.names = F)
 
+## Least Disturbed sites.
 ##Less-disturbed sites selected  by filtering for proportion local CRP
 # ldsites <- df %>%
 #   filter(prop_local_CRP >= 0.50)
