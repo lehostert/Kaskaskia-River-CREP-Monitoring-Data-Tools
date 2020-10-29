6f79fcadc5a25edc119e273e9ebf3a96845f7584
Lauren E. Hostert <laurenehostert@gmail.com>
2020-10-29 13:49:35 -0500

    Add 2019 sample shipment

--- Invertebrate_Lab_Processing/Macroinvertebrates_Subsampling_Random_Cells.R
+++ Invertebrate_Lab_Processing/Macroinvertebrates_Subsampling_Random_Cells.R
@@ -1,19 +1,18 @@
 library(dplyr)
 
+year <- 2020
 cells <- {1:30}
 
 cells
 
-set.seed(2019)
+set.seed(year)
 # sites <-  cells %>%
 #   group_by(basin, size_class, CRP_class) %>%
 #   sample(cells)
 
 InvertCells<-replicate(100, sample(cells))
+write.csv(t(InvertCells), file = paste0("macroinvertebrate_subsampling_cells_",year,".csv"))
 
-
-write.csv(t(InvertCells), file = "macroinvertebrate_subsampling_cells_2019.csv") 
-
-set.seed(2018)
+set.seed(year)
 InvertCells<-replicate(30, sample(cells))
-write.csv(t(InvertCells), file = "macroinvertebrate_subsubsampling_cells_2018") 
+write.csv(t(InvertCells), file = paste0("macroinvertebrate_subsubsampling_cells_",year,".csv")) 
