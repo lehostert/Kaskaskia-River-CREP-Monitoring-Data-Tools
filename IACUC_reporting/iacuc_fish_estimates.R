03fded69b7e18a61846d686d96b8f9fb3a366df1
Lauren E. Hostert <laurenehostert@gmail.com>
2020-10-29 18:07:52 -0500

    Add fish data from DB being summaries of fish mortality

--- IACUC_reporting/iacuc_fish_estimates.R
+++ IACUC_reporting/iacuc_fish_estimates.R
@@ -5,55 +5,76 @@ library(DBI)
 
 ### with odbc
 odbcListDrivers() # to get a list of the drivers your computer knows about 
-con <- dbConnect(odbc::odbc(), "2019_CREP_Database")
-dbListTables(con) # To get the list of tables in the database
+con <- dbConnect(odbc::odbc(), "2019_CREP_Database") #2019_CREP_Database is what you named the current CREP_Database when you set up the driver
+dbListTables(con) # To get the list of tables in the database #There is a bunch of additional stuff in here that I don't know what it does
 
-odbc_result <- DBI::dbReadTable(con, "Fish_Abundance") # The dbReadTable() command will return the data table from the database as class: "data.frame"
-result_dbi<- as_tibble(odbc_result) # Use as_tibble() turn the dataframe as a tibble dataframe to continue with queries using the dplyr syntax
-result_dplyr <- as_tibble(tbl(con, "Fish_Abundance")) # OR directly return the data table from the database as a tibble
+fish <- as_tibble(tbl(con, "Fish_Abundance")) # Directly return the data table from the database as a tibble
 
-#### Conducting Queries ####
-## Using DBI and SQL 
-fish_locations_sql_dbi <- DBI::dbGetQuery(con, "SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date, 
-                                          Established_Locations.Site_Type, Established_Locations.Latitude, 
-                                          Established_Locations.Longitude, Established_Locations.Stream_Name 
-                                       FROM Fish_Abundance LEFT JOIN Established_Locations 
-                                       ON (Fish_Abundance.PU_Gap_Code = Established_Locations.PU_Gap_Code) 
-                                       AND (Fish_Abundance.Reach_Name = Established_Locations.Reach_Name)") 
+# Summary of all of the fish data by sampling event
+summary_fish <- fish %>%
+  mutate(year = lubridate::year(Event_Date)) %>% 
+  group_by(Reach_Name, year) %>% 
+  summarise(
+    total_fish = sum(Fish_Species_Count)
+  ) %>% 
+  ungroup()
 
-# You can use dbGetQuery() for SELECT queries only not queries that involve manipulation. 
+# Summary of all of the fish data by year "How many fish over how many years"
+summary_fish_ave <- fish %>%
+  mutate(year = lubridate::year(Event_Date)) %>% 
+  group_by(year) %>% 
+  summarise(
+    total_fish = sum(Fish_Species_Count),
+    total_sites = n_distinct(Reach_Name)
+  )
+
+se <- function(x) {
+  sqrt(var(x)/length(x))
+}
+
+#per year summary
+per_year_summary <- summarise(summary_fish_ave, 
+          mean = mean(total_fish),
+          sd = sd(total_fish),
+          sem = se(total_fish),
+          n = n()
+          )
+
+#per site summary 
+per_site_summary <- summarise(summary_fish, 
+                              mean = mean(total_fish),
+                              sd = sd(total_fish),
+                              sem = se(total_fish),
+                              n = n(),
+                              max = max(total_fish),
+                              min = min(total_fish)
+                              )
+
+f19 <- readxl::read_xlsx(path = "~/CREP/Permits/2019/2019_Fish_Permit_Summary.xlsx", col_names = T)
+f18 <- readxl::read_xlsx(path = "~/CREP/Permits/2018/2018_Fish_Permit_Summary.xlsx", col_names = T)
 
 
+f18_fix <- rename(f18, c(Count= Number, Release_status = Disposition))
 
-## Using dplyr and SQL 
-fish_locations_sql_dplyr <- as_tibble(tbl(con, sql("SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date, 
-                                          Established_Locations.Site_Type, Established_Locations.Latitude, 
-                                          Established_Locations.Longitude, Established_Locations.Stream_Name 
-                                       FROM Fish_Abundance LEFT JOIN Established_Locations 
-                                       ON (Fish_Abundance.PU_Gap_Code = Established_Locations.PU_Gap_Code) 
-                                       AND (Fish_Abundance.Reach_Name = Established_Locations.Reach_Name)")))
-# tbl() this is a generic method for creating a table from a datasource. In this case the source is our database connection called "con"
-# sql() allows you to write SQL queries directly
-# as_tibble() will turn the resulting object into a tibble data.frame
-# If you remove the as_tibble() function you will get a result that is class ["tbl_ACCESS" "tbl_dbi"    "tbl_sql"    "tbl_lazy"   "tbl"]
-# As a tibble you will be able to manipulate it using any 
-# TODO can you use sql() for manipulating queries??
+mort <- bind_rows(f19, f18_fix) %>%
+  mutate(Year = replace_na(lubridate::year(Event_Date), 2018)) %>% 
+  group_by(Year, Release_status)%>% 
+  summarise(
+    total_fish = sum(Count))%>% 
+  ungroup()
 
-## Using dplyr and tidyverse syntax only
-fish_locations_dplyr <- as_tibble(tbl(con, "Fish_Abundance")) %>% 
-  left_join(as_tibble(tbl(con, "Established_Locations")), by = c("PU_Gap_Code", "Reach_Name")) %>% 
-  select(PU_Gap_Code, Reach_Name, Event_Date, Site_Type, Latitude, Longitude, Stream_Name) %>% 
-  distinct()
+#TODO Find a way to get the ratio of mortality to released total for 2018 & 2019 as well as mort ratio by year. 
 
+# mort %>% group_by(Release_status) %>% 
+#   summarise(
+#   total_fish = sum(Count)
+# )
+  
+combo <- bind_rows(f19, f18_fix) 
 
-# You can either pull tables out one by one and then join them or use the tbl() function to pull tables from the DB and join them in the same lines of code
-# NOTE that if you don't specify the "by" for the `join` dplyr will automatically use all of the fields that are the same between the two tables. 
-# In this case removing the "by" argument would result in the same dataframe. 
-
-# NOTE: fish_locations_sql_dbi, fish_locations_sql_dplyr, and fish_locations_dplyr all have the same information (ie. number of observations and variables) 
-# but they are  different classes. 
-# The DBI query is just a class "data.frame" but the dplyr queries have explicitly been turned into class "tbl_df" "tbl" "data.frame"
-
+summarise(combo, 
+    total_fish = sum(Count)
+  )
 
 #### When you are done close your database connection ####
 dbDisconnect(con)
