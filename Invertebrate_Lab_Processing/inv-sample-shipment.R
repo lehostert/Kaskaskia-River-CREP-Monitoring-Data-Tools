004d34b4c7b30afd099f8b10a4cc767e6ab7524b
Lauren E. Hostert <laurenehostert@gmail.com>
2021-01-14 18:33:42 -0600

    Add specimen number information to shipping manifest

--- Invertebrate_Lab_Processing/inv-sample-shipment.R
+++ Invertebrate_Lab_Processing/inv-sample-shipment.R
@@ -1,9 +1,12 @@
 library(tidyverse)
 
 network_prefix <- "//INHS-Bison" #Lauren's Desktop PC
+year <- "2020"
 
-inv2020 <- read_csv(file = paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Data/Data_IN/DB_Ingest/INV_2019.csv"))
+### Read in data going into DB
+inv2020 <- read_csv(file = paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Data/Data_IN/DB_Ingest/INV_", year,".csv"))
 
+### Clean and reformat data
 inv <- inv2020 %>% 
   select(PU_Gap_Code, Reach_Name, Event_Date, Jab_Habitat, Jab_Collector) %>% 
   group_by(PU_Gap_Code, Reach_Name, Event_Date, Jab_Collector) %>% 
@@ -11,4 +14,17 @@ inv <- inv2020 %>%
   summarize(Habitat = toString(Jab_Habitat)) %>% 
   ungroup
 
-write_csv(inv, path = "~/CREP/External_Lab/EcoAnalysts Invert Tables/2019/Invert_Sample_Labels_2019.csv")
+inv$Collector <- "lhostert"
+inv$Reach_Name <- stringr::str_to_lower(inv$Reach_Name)
+
+## Read in data from lab processing
+inv_online <- readxl::read_excel(path = paste0("~/CREP/External_Lab/EcoAnalysts Invert Tables/2020/macroinvertebrate_labprocessing_", year,".xlsx"))
+inv_sum <- inv_online %>% 
+  select(PUGap_Code, Site_Name, Event_Date, INV_Date, "# SubsampleVials...12") %>% 
+  rename(SubsampleVials = "# SubsampleVials...12",) 
+  
+inv_final <- full_join(inv, inv_sum, by = c("PU_Gap_Code" = "PUGap_Code", "Event_Date" = "INV_Date", "Reach_Name" = "Site_Name")) %>% 
+  rename(INV_Date = Event_Date, Event_Date = Event_Date.y)
+
+
+write_csv(inv, path = paste0("~/CREP/External_Lab/EcoAnalysts Invert Tables/", year,"/Invert_Sample_Labels_", year,".csv"))
