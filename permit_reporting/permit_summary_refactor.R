3e40ca9c1ac334b7c8d65d69c6333d2e5285e14a
Lauren E. Hostert <laurenehostert@gmail.com>
2021-01-27 18:38:32 -0600

    Update fish lists to work together and comment

--- permit_reporting/permit_summary_refactor.R
+++ permit_reporting/permit_summary_refactor.R
@@ -31,7 +31,10 @@ fish_summary$Release_status <- str_replace(fish_summary$Release_status, "alive",
 fish_summary$Release_status <- str_replace(fish_summary$Release_status, "voucher", "Donated to INHS")
 fish_summary$Release_status <- str_replace(fish_summary$Release_status, "Voucher", "Donated to INHS")
 
-#Fix your mess
+#the voucher information is going to be an issue because it will have treated those with "Voucher" and those with "voucher" as a separate type for the count above 
+
+# Fix your mess
+# TODO Update the Fish trait matrix base to be consistent and then add to to the excel sheet. 
 fish_summary$Fish_Species_Common <- str_replace(fish_summary$Fish_Species_Common, "Carp", "Common Carp")
 fish_summary$Fish_Species_Common <- str_replace(fish_summary$Fish_Species_Common, "Mosquitofish", "Western mosquitofish")
 fish_summary$Fish_Species_Scientific <- str_replace(fish_summary$Fish_Species_Scientific, "Lythrurus umbratilus", "Lythrurus umbratilis")
@@ -40,7 +43,7 @@ fish_summary$Fish_Species_Scientific <- str_replace(fish_summary$Fish_Species_Sc
 fish_summary$Fish_Species_Scientific <- str_replace(fish_summary$Fish_Species_Scientific, "Catostomus commersoni", "Catostomus commersonii")
 
 
-### Read in 2020 location data
+### Read in Location data
 
 location <- read.csv(file = paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Data/Data_IN/Sites_",sampling_year,".csv"), header = T, na.strings = c("NA", ""))
 location$Event_Date  <- as.Date(location$Event_Date, format = "%m/%d/%Y")
@@ -56,21 +59,24 @@ location_summary$Reach_Name <- stringr::str_remove(location_summary$Reach_Name, 
 
 ### Join Fish and Location Data
 permit_summary <- right_join(location_summary, fish_summary, by = c("PU_Gap_Code", "Reach_Name", "Event_Date")) %>% 
-  select(-c(Reach_Name))
+  select(-c(Reach_Name, Species_Code))
 
 ### Write it to a file
 
 writexl::write_xlsx(permit_summary, path = paste0("~/CREP/Permits/",sampling_year,"/",sampling_year,"_Fish_Permit_Summary.xlsx"))
 
-#### Check for strange fish
+#### Check for T&E Reportable Species
+# Combine fish summary and il_fish information to get a list of fish that are just state threatened or endangered
 
 il_fish_path_pc <- "//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Fish_Trait_Matrix_Base.csv"
 il_fish_spp <- read.csv(il_fish_path_pc, header = T, stringsAsFactors = F, na ="")
 
-# Combine fish summary and il_fish impformation to get a list of fish that are just state threatened or exdangered
 
 fish_summary$Fish_Species_Common <- stringr::str_to_lower(fish_summary$Fish_Species_Common)
 fish_check <- left_join(fish_summary, il_fish_spp, by = c("Species_Code"= "Fish_Species_Code", "Fish_Species_Common", "Fish_Species_Scientific"))
 
-fish_check_na <- fish_check %>% 
-  filter(is.na(Historic_Family))
+fish_check_IL <- fish_check %>% 
+  filter(!is.na(IL_Status))
+
+fish_check_USA <- fish_check %>% 
+  filter(!is.na(Federal_Status))
