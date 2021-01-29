a151f1da831554d17e414e60518f3ecf54ba42e8
Lauren E. Hostert <laurenehostert@gmail.com>
2021-01-29 17:01:06 -0600

    Add section to append new data to current database

--- Combine_Data_IN/db_ingest.R
+++ Combine_Data_IN/db_ingest.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(odbc)
 library(DBI)
+library(docstring)
 
 network_prefix <- if_else(as.character(Sys.info()["sysname"]) == "Windows", "//INHS-Bison", "/Volumes")
 
@@ -12,12 +13,13 @@ con <- dbConnect(odbc::odbc(), "Testing_Database")
 # con <- dbConnect(odbc::odbc(), "2019_CREP_Database")
 dbListTables(con) # To get the list of tables in the database
 
-dbWriteTable(con, "Water_Chemistry_Field", SWC_2020, batch_rows = 1, append = TRUE)
 
-##TODO test this
 dbAppendTable(conn = con, name = "Habitat_Discharge", value = DSC_2020,  batch_rows = 1)
-dbWriteTable(con, "Habitat_Discharge", DSC_2020, batch_rows = 1, append = TRUE)
-
+dbAppendTable(conn = con, name = "Fish_Metadata", value = FMD, batch_rows = 1)
+dbAppendTable(conn = con, name = "Habitat_IHI", value = IHI_2020, batch_rows = 1)
+dbAppendTable(conn = con, name = "Habitat_QHEI", value = QHEI_2020, batch_rows = 1)
+dbAppendTable(conn = con, name = "Invert_Metadata_Field", value = INV_2020, batch_rows = 1)
+dbAppendTable(conn = con, name = "Water_Chemistry_Field", value = SWC_2020, batch_rows = 1)
 
 ## This is based on GitHub Comments for issues #263 of the odbc package. but you should continue reading to see
 ## if dbAppendTable is a "safer" option for what you want to do because there is no way to set append to be FALSE with dbAppendTable but there is for 
@@ -40,58 +42,23 @@ dbWriteTable(con, "Habitat_Discharge", DSC_2020, batch_rows = 1, append = TRUE)
 
 dbDisconnect(con)
 
-## Keep this for when you are creating a script to ingest all of the data into the DB then auto move them to the OUT folder
-## file.rename(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Data/Data_IN/DB_Ingest/test.csv"), paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Data/Data_OUT/DB_Ingested/test.csv"))
+move_in_to_out <- function(dat_type, sampling_year) {
+  #' After appending data to DB ingested tables are move from Data_IN folder to Data_OUT folder. 
+  #'
+  #' @param dat_type 3-4 letter code used in template files for the data type you want to combine.
+  #' @param sampling_year numeric "YYYY" representation of the sampling year of interest.
+  #'
 
+file.rename(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Data/Data_IN/DB_Ingest/",dat_type, "_",sampling_year, ".csv"), 
+            paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Data/Data_OUT/DB_Ingested/",dat_type, "_",sampling_year, ".csv"))
 
-#####################################################
-#### Below here be dragons ####
-odbc_result <- DBI::dbReadTable(con, "Fish_Abundance") # The dbReadTable() command will return the data table from the database as class: "data.frame"
-result_dbi<- as_tibble(odbc_result) # Use as_tibble() turn the dataframe as a tibble dataframe to continue with queries using the dplyr syntax
-result_dplyr <- as_tibble(tbl(con, "Fish_Abundance")) # OR directly return the data table from the database as a tibble
+}
 
-#### Conducting Queries ####
-## Using DBI and SQL 
-fish_locations_sql_dbi <- DBI::dbGetQuery(con, "SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date, 
-                                          Established_Locations.Site_Type, Established_Locations.Latitude, 
-                                          Established_Locations.Longitude, Established_Locations.Stream_Name 
-                                       FROM Fish_Abundance LEFT JOIN Established_Locations 
-                                       ON (Fish_Abundance.PU_Gap_Code = Established_Locations.PU_Gap_Code) 
-                                       AND (Fish_Abundance.Reach_Name = Established_Locations.Reach_Name)") 
+move_in_to_out("DSC", 2020)
+move_in_to_out("FMD", 2020)
+move_in_to_out("IHI", 2020)
+move_in_to_out("QHEI", 2020)
+move_in_to_out("INV", 2020)
+move_in_to_out("SWC", 2020)
 
-# You can use dbGetQuery() for SELECT queries only not queries that involve manipulation. 
-
-
-
-## Using dplyr and SQL 
-fish_locations_sql_dplyr <- as_tibble(tbl(con, sql("SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date, 
-                                          Established_Locations.Site_Type, Established_Locations.Latitude, 
-                                          Established_Locations.Longitude, Established_Locations.Stream_Name 
-                                       FROM Fish_Abundance LEFT JOIN Established_Locations 
-                                       ON (Fish_Abundance.PU_Gap_Code = Established_Locations.PU_Gap_Code) 
-                                       AND (Fish_Abundance.Reach_Name = Established_Locations.Reach_Name)")))
-# tbl() this is a generic method for creating a table from a datasource. In this case the source is our database connection called "con"
-# sql() allows you to write SQL queries directly
-# as_tibble() will turn the resulting object into a tibble data.frame
-# If you remove the as_tibble() function you will get a result that is class ["tbl_ACCESS" "tbl_dbi"    "tbl_sql"    "tbl_lazy"   "tbl"]
-# As a tibble you will be able to manipulate it using any 
-# TODO can you use sql() for manipulating queries??
-
-## Using dplyr and tidyverse syntax only
-fish_locations_dplyr <- as_tibble(tbl(con, "Fish_Abundance")) %>% 
-  left_join(as_tibble(tbl(con, "Established_Locations")), by = c("PU_Gap_Code", "Reach_Name")) %>% 
-  select(PU_Gap_Code, Reach_Name, Event_Date, Site_Type, Latitude, Longitude, Stream_Name) %>% 
-  distinct()
-
-
-# You can either pull tables out one by one and then join them or use the tbl() function to pull tables from the DB and join them in the same lines of code
-# NOTE that if you don't specify the "by" for the `join` dplyr will automatically use all of the fields that are the same between the two tables. 
-# In this case removing the "by" argument would result in the same dataframe. 
-
-# NOTE: fish_locations_sql_dbi, fish_locations_sql_dplyr, and fish_locations_dplyr all have the same information (ie. number of observations and variables) 
-# but they are  different classes. 
-# The DBI query is just a class "data.frame" but the dplyr queries have explicitly been turned into class "tbl_df" "tbl" "data.frame"
-
-
-#### When you are done close your database connection ####
-dbDisconnect(con)
+# TODO make the ingest portion of this code more generic. Remove years from object names for "value" and to ingest data from .csv  and not from objects saved in R
