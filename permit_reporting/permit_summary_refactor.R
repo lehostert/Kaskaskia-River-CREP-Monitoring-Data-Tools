994862ff5ed779e35307145c885c895fe101d283
Lauren E. Hostert <laurenehostert@gmail.com>
2022-01-31 11:38:21 -0600

    Update permit summary to reflect 2021 report

--- permit_reporting/permit_summary_refactor.R
+++ permit_reporting/permit_summary_refactor.R
@@ -85,3 +85,66 @@ fish_check_IL <- fish_check %>%
 
 fish_check_USA <- fish_check %>% 
   filter(!is.na(Federal_Status))
+
+### Refactor #### 
+
+#### Connect to Database using odbc####
+library(odbc)
+library(DBI)
+odbcListDrivers() # to get a list of the drivers your computer knows about, be sure there is one for MS Access
+# con <- dbConnect(odbc::odbc(), "Testing_Database")
+con <- dbConnect(odbc::odbc(), "2019_CREP_Database")
+options(odbc.batch_rows = 1) # Must be defined as 1 for Access batch writing or appending to tables.
+dbListTables(con) # To get the list of tables in the database
+
+loc <- as_tibble(tbl(con, "Established_Locations"))
+fmd <- as_tibble(tbl(con, "Fish_Metadata"))
+fish <- as_tibble(tbl(con, "Fish_Abundance"))
+
+dbDisconnect(con)
+
+
+fish$Reach_Name <- stringr::str_remove(fish$Reach_Name, "[:blank:]*$")
+fish$PU_Gap_Code  <- stringr::str_remove(fish$PU_Gap_Code, "[:blank:]*$")
+
+fish$Release_status <- str_replace(fish$Release_status, "mortality", "Destroyed")
+fish$Release_status <- str_replace(fish$Release_status, "alive", "Released")
+fish$Release_status <- str_replace(fish$Release_status, "voucher", "Destroyed")
+fish$Release_status <- str_replace(fish$Release_status, "Voucher", "Destroyed")
+fish$Release_status <- str_replace(fish$Release_status, "INHS", "Donated to INHS")
+fish$Release_status  <- stringr::str_remove(fish$Release_status, "[:blank:]*$")
+
+fish_data <- fish %>%
+  drop_na(Fish_Species_Code) %>% 
+  mutate(Release_status = replace_na(Release_status, "Released")) %>% #TODO Fix Db Fish Entries so blanks have 'Released'
+  filter(lubridate::year(Event_Date) == sampling_year) %>% 
+  group_by(PU_Gap_Code, Reach_Name, Fish_Date, Fish_Species_Code, Release_status) %>% 
+  summarise(Fish_Species_Count = n()) %>% 
+  ungroup()
+
+
+#### Check IL and Federal Status
+
+il_fish_path <- paste0(network_path, "/Analysis/Fish/Fish_Trait_Matrix_Base.csv")
+il_fish_list <- read.csv(il_fish_path, header = T, stringsAsFactors = F, na ="")
+
+fish_check<- fish_data %>% 
+  left_join(il_fish_list)
+
+fish_check_IL <- fish_check %>% 
+  filter(!is.na(IL_Status))
+
+fish_check_USA <- fish_check %>% 
+  filter(!is.na(Federal_Status))
+
+# TODO Print Error Message for if there any fish from the IL or FED T&E list
+
+if_else(nrow(fish_check_IL) > 0, print("Submit Additional Report for T&E Species"), print(""))
+if_else(nrow(fish_check_USA) > 0, print("Submit Additional Report for T&E Species"), print(""))
+
+#### Write Permit Summary Report ####
+permit <- fish_check %>% 
+  left_join(loc) %>% 
+  mutate(Collection_Method = "electrofishing") %>% 
+  select(Fish_Species_Common, Latitude, Longitude, Observation_Date = Fish_Date, Abundance = Fish_Species_Count, 
+         Collection_Method, Disposition = Release_status)
