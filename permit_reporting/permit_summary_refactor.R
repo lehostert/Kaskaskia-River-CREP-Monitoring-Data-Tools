0f01e5ef2ad18374533dac08cbfbc480c4e092ee
Lauren Hostert <40471257+lehostert@users.noreply.github.com>
2022-02-02 10:57:51 -0600

    Permit (#18)
    * Refactor annual permit report to reflect 2021 report style

--- permit_reporting/permit_summary_refactor.R
+++ permit_reporting/permit_summary_refactor.R
@@ -1,84 +1,83 @@
 library(tidyverse)
 library(writexl)
 
-network_prefix <- if_else(as.character(Sys.info()["sysname"]) == "Windows", "//INHS-Bison", "/Volumes")
-# network_prefix <- "//INHS-Bison" #Lauren's Desktop PC
-
-sampling_year <- 2020
-
-#### Read in fish data and turn into abundance data ####
-
-fish <- read.csv(file = paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Data/Data_IN/DB_Ingest/FSH_",sampling_year,".csv"), header = T, na.strings = "")
-fish$Event_Date <- as.Date(fish$Event_Date)
+network_prefix <- if_else(as.character(Sys.info()["sysname"]) == "Windows", "//INHS-Bison.ad.uillinois.edu", "/Volumes")
+network_path <- paste0(network_prefix, "/ResearchData/Groups/StreamEcologyLab/CREP")
 
 
-#### Fish from single line list then collapse into single line for each species type with count per site####
+sampling_year <- 2021
 
-fish_summary <- fish %>% 
-  drop_na(Species_Code) %>% 
-  mutate(across(where(is.character), ~na_if(., "no"))) %>% 
-  mutate(Release_status = replace_na(Release_status, "alive"))%>%
-  group_by(PU_Gap_Code, Reach_Name, Event_Date, Species_Code, Fish_Species_Common, Fish_Species_Scientific, Release_status) %>% 
-  summarise(Fish_Species_Count = n())
+#### Connect to Database using odbc####
+library(odbc)
+library(DBI)
+odbcListDrivers() # to get a list of the drivers your computer knows about, be sure there is one for MS Access
+# con <- dbConnect(odbc::odbc(), "Testing_Database")
+con <- dbConnect(odbc::odbc(), "2019_CREP_Database")
+options(odbc.batch_rows = 1) # Must be defined as 1 for Access batch writing or appending to tables.
+dbListTables(con) # To get the list of tables in the database
 
-# fish_summary$Reach_Name <- stringr::str_replace(fish_summary$Reach_Name, "copper[:blank:]|copper|Copper|copper[:digit:]","Copper ") %>% 
-#   stringr::str_replace(fish_summary$Reach_Name, "kasky[:blank:]|Kasky[:blank:]|Kasky","kasky")
+loc <- as_tibble(tbl(con, "Established_Locations"))
+fmd <- as_tibble(tbl(con, "Fish_Metadata"))
+fish <- as_tibble(tbl(con, "Fish_Abundance"))
 
-fish_summary$PU_Gap_Code <- stringr::str_to_lower(fish_summary$PU_Gap_Code)
-fish_summary$Reach_Name <-  stringr::str_to_lower(fish_summary$Reach_Name)
-fish_summary$Reach_Name <- stringr::str_remove(fish_summary$Reach_Name, "[:blank:]")
+dbDisconnect(con)
 
-fish_summary$Release_status <- str_replace(fish_summary$Release_status, "mortality", "Destroyed")
-fish_summary$Release_status <- str_replace(fish_summary$Release_status, "alive", "Released")
-fish_summary$Release_status <- str_replace(fish_summary$Release_status, "voucher", "Donated to INHS")
-fish_summary$Release_status <- str_replace(fish_summary$Release_status, "Voucher", "Donated to INHS")
-
-#the voucher information is going to be an issue because it will have treated those with "Voucher" and those with "voucher" as a separate type for the count above 
-
-# Fix your mess
-# TODO Update the Fish trait matrix base to be consistent and then add to to the excel sheet. 
-fish_summary$Fish_Species_Common <- str_replace(fish_summary$Fish_Species_Common, "Carp", "Common Carp")
-fish_summary$Fish_Species_Common <- str_replace(fish_summary$Fish_Species_Common, "Mosquitofish", "Western mosquitofish")
-fish_summary$Fish_Species_Scientific <- str_replace(fish_summary$Fish_Species_Scientific, "Lythrurus umbratilus", "Lythrurus umbratilis")
-fish_summary$Fish_Species_Scientific <- str_replace(fish_summary$Fish_Species_Scientific, "Esox americanus", "Esox americanus vermiculatus")
-fish_summary$Fish_Species_Scientific <- str_replace(fish_summary$Fish_Species_Scientific, "Notropis ludibundus", "Notropis stramineus")
-fish_summary$Fish_Species_Scientific <- str_replace(fish_summary$Fish_Species_Scientific, "Catostomus commersoni", "Catostomus commersonii")
+#### Clean up Data from Database; There are issues was additional spaces after text fields in the Fish Tables ####
+fish$Reach_Name <- stringr::str_remove(fish$Reach_Name, "[:blank:]*$")
+fish$PU_Gap_Code  <- stringr::str_remove(fish$PU_Gap_Code, "[:blank:]*$")
+fish$Release_status  <- stringr::str_remove(fish$Release_status, "[:blank:]*$")
 
 
-### Read in Location data
+fish_voucher_21 <- fish %>% 
+  filter(lubridate::year(Event_Date) == sampling_year,
+         Release_status == "voucher")
 
-location <- read.csv(file = paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Data/Data_IN/Sites_",sampling_year,".csv"), header = T, na.strings = c("NA", ""))
-location$Event_Date  <- as.Date(location$Event_Date, format = "%m/%d/%Y")
+fish$Release_status <- str_replace(fish$Release_status, "mortality", "Released")
+fish$Release_status <- str_replace(fish$Release_status, "alive", "Released")
+fish$Release_status <- str_replace(fish$Release_status, "voucher", "Destroyed")
+fish$Release_status <- str_replace(fish$Release_status, "Voucher", "Destroyed")
+fish$Release_status <- str_replace(fish$Release_status, "INHS", "Donated to INHS")
+fish$Release_status  <- stringr::str_remove(fish$Release_status, "[:blank:]*$")
 
-location_summary <- location %>%
-  drop_na(PU_Gap_Code) %>%
-  select(PU_Gap_Code, Reach_Name, Latitude, Longitude, Stream_Name, Event_Date)
+####  Consolidate fish data by sampling event and release status ####
 
-location_summary$PU_Gap_Code <- stringr::str_to_lower(location_summary$PU_Gap_Code)
-location_summary$Reach_Name <-  stringr::str_to_lower(location_summary$Reach_Name)
-location_summary$Reach_Name <- stringr::str_remove(location_summary$Reach_Name, "[:blank:]")
-
-
-### Join Fish and Location Data
-permit_summary <- right_join(location_summary, fish_summary, by = c("PU_Gap_Code", "Reach_Name", "Event_Date")) %>% 
-  select(-c(Reach_Name, Species_Code))
-
-### Write it to a file
-
-writexl::write_xlsx(permit_summary, path = paste0("~/CREP/Permits/",sampling_year,"/",sampling_year,"_Fish_Permit_Summary.xlsx"))
-
-#### Check for T&E Reportable Species
-# Combine fish summary and il_fish information to get a list of fish that are just state threatened or endangered
-
-il_fish_path_pc <- "//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Fish_Trait_Matrix_Base.csv"
-il_fish_spp <- read.csv(il_fish_path_pc, header = T, stringsAsFactors = F, na ="")
+fish_data <- fish %>%
+  drop_na(Fish_Species_Code) %>% 
+  mutate(Release_status = replace_na(Release_status, "Released")) %>% #TODO Fix Db Fish Entries so blanks have 'Released'
+  filter(lubridate::year(Event_Date) == sampling_year) %>%
+  group_by(PU_Gap_Code, Reach_Name, Fish_Date, Fish_Species_Code, Release_status) %>% 
+  summarise(Fish_Species_Count = n()) %>% 
+  ungroup()
 
 
-fish_summary$Fish_Species_Common <- stringr::str_to_lower(fish_summary$Fish_Species_Common)
-fish_check <- left_join(fish_summary, il_fish_spp, by = c("Species_Code"= "Fish_Species_Code", "Fish_Species_Common", "Fish_Species_Scientific"))
+#### Check IL and Federal Status ####
+
+il_fish_path <- paste0(network_path, "/Analysis/Fish/Fish_Trait_Matrix_Base.csv")
+il_fish_list <- read.csv(il_fish_path, header = T, stringsAsFactors = F, na ="")
+
+fish_check<- fish_data %>% 
+  left_join(il_fish_list)
 
 fish_check_IL <- fish_check %>% 
   filter(!is.na(IL_Status))
 
 fish_check_USA <- fish_check %>% 
   filter(!is.na(Federal_Status))
+
+# TODO Print Error Message for if there any fish from the IL or FED T&E list
+
+if_else(nrow(fish_check_IL) > 0, print("Submit Additional Report for T&E Species"), print(""))
+if_else(nrow(fish_check_USA) > 0, print("Submit Additional Report for T&E Species"), print(""))
+
+#### Write Permit Summary Report ####
+permit <- fish_check %>% 
+  left_join(loc) %>% 
+  mutate(Collection_Method = "electrofishing",
+         Fish_Species_Common_Name = stringr::str_to_title(Fish_Species_Common)) %>% 
+  select(Fish_Species_Common_Name, Latitude, Longitude, Observation_Date = Fish_Date, Abundance = Fish_Species_Count, 
+         Collection_Method, Disposition = Release_status) %>% 
+  group_by(Latitude, Longitude) %>% 
+  arrange(Observation_Date, Fish_Species_Common_Name) %>% 
+  ungroup()
+
+write_csv(permit, path = "~/CREP/Permits/2021/2021_Fish_Permit_Summary.csv")
